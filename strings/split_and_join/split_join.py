cd9904e00c2645338077242bd0a9d7fd089a8b6e
Leonardo Villalobos <levillalobos@csumb.edu>
2021-12-28 13:21:13 -0800

    Add split_join.py and info.pdf for file.

---
+++ strings/split_and_join/split_join.py
@@ -0,0 +1,24 @@
+"""
+You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+Function description:
+Complete the split_and_join function in the editor below.
+split_and_join has the following parameters:
+    string line: a string of space-separated words
+Returns:
+    string: the resulting string
+
+Further details are provided in the info.pdf file provided within this directory.
+"""
+
+
+def split_and_join (line: str) -> str:
+    to_list = line.split ()
+    add_hyphen = "-".join (to_list)
+    return add_hyphen
+
+
+if __name__ == '__main__':
+    line = input ()
+    result = split_and_join (line)
+    print (result)
